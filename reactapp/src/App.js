6fe1b613026d1b7348a86b31eb2772cb873599c9
bizhacks24 <bizhacks24@gmail.com>
2023-08-25 12:23:46 +0800

    my 4th attemp in using git

---
+++ reactapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <strong>Upload React App to Hostinger VPS for testing ...  </strong>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
